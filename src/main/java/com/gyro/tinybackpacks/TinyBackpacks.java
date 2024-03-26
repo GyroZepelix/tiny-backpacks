b0792582a7ade7def0b4b01f028fd04867ce15f4
GyroZepelix <gamingskullgamer@gmail.com>
2024-03-26 18:19:24 +0100

    Removed Config

--- src/main/java/com/gyro/tinybackpacks/TinyBackpacks.java
+++ src/main/java/com/gyro/tinybackpacks/TinyBackpacks.java
@@ -40,23 +40,7 @@ public class TinyBackpacks
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        // Register the commonSetup method for modloading
-        modEventBus.addListener(this::commonSetup);
-
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
-        if (Config.logDirtBlock)
-            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
-
-        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
-
-        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
     }
 }
